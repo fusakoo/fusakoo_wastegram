10d0c68c0e2910fc44a76c12647d2bf1efff61f4
fusakoo <obatafusako@gmail.com>
2022-08-11 15:53:31 -0700

    Implement Sentry crash reporting

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,26 @@
+import 'package:flutter/foundation.dart' as Foundation;
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:wastegram/wastegram.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static Future<void> reportError(dynamic error, dynamic stackTrace) async {
+    // if (Foundation.kDebugMode) {
+    //   print(stackTrace);
+    //   return;
+    // }
+    final sentryId =
+        await Sentry.captureException(
+          error, 
+          stackTrace: stackTrace
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    // throw StateError('Test error');
     return MaterialApp(
       title: 'Wastegram',
       theme: ThemeData.dark(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'wastegram.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'wastegram.dart';
+
+const dsnURL = 'https://aef87f78e9414b5d88e50fa501623889@o1355556.ingest.sentry.io/6640180';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = dsnURL;
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const MyApp()),
+  );
 }
